937bab325756c43f4fcbaafdc38c1f625830b596
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2016-09-30 17:14:36 +0200

    Adds datetimepicker wip

--- onegov/org/assets/js/datetimepicker.js
+++ onegov/org/assets/js/datetimepicker.js
@@ -1,25 +1,29 @@
 var datetimepicker_i18n = {
     de_CH: {
         dayOfWeekStart: 1, // Monday
-        format: 'd.m.Y',
+        dateformat: 'd.m.Y',
+        datetimeformat: 'd.m.Y h:m',
         placeholder: 'TT.MM.JJJJ',
         lang: 'de'
     },
     it_CH: {
         dayOfWeekStart: 1,
-        format: 'd.m.Y',
+        dateformat: 'd.m.Y',
+        datetimeformat: 'd.m.Y h:m',
         placeholder: 'gg.mm.aaaa',
         lang: 'it'
     },
     fr_CH: {
         dayOfWeekStart: 1,
-        format: 'd.m.Y',
+        dateformat: 'd.m.Y',
+        datetimeformat: 'd.m.Y h:m',
         placeholder: 'jj.mm.aaaa',
         lang: 'fr'
     },
     rm_CH: {
         dayOfWeekStart: 1,
-        format: 'd-m-Y',
+        dateformat: 'd-m-Y',
+        datetimeformat: 'd.m.Y h:m',
         placeholder: 'dd-mm-oooo',
         lang: 'rm'
     }
@@ -44,87 +48,130 @@ var get_locale = function() {
     }
 };
 
-// load the datetimepicker for date inputs if the browser does not support it
-if (!Modernizr.inputtypes.date) {
-    var _locale = get_locale();
+var attach_button = function(input) {
+    var large_column = 'small-11';
+    var small_column = 'small-1';
 
-    $('input[type=date]').each(function() {
-        var input = $(this);
-        var large_column = 'small-11';
-        var small_column = 'small-1';
+    if (input.is('.small')) {
+        large_column = 'small-10';
+        small_column = 'small-2';
+    }
 
-        if (input.is('.small')) {
-            large_column = 'small-10';
-            small_column = 'small-2';
+    // inject a button with which to launch the datetime picker
+    var button = $('<a href="#" role="presentation" tabindex="-1" class="button secondary postfix datetimepicker"><i class="fa fa-calendar"></i></a>');
+    var grid = $([
+        '<div class="row collapse">',
+        '<div class="' + large_column + ' columns"></div>',
+        '<div class="' + small_column + ' columns"></div>',
+        '</div>'
+    ].join(''));
+
+    var visible = false;
+
+    grid.insertBefore(input);
+    input.detach().appendTo(grid.find('.' + large_column));
+    button.appendTo(grid.find('.' + small_column));
+
+    // hide/show the datetime picker when clicking on the button
+    button.click(function(e) {
+        if (visible) {
+            input.datetimepicker('hide');
+        } else {
+            input.datetimepicker('show');
         }
 
-        // inject a button with which to launch the datetime picker
-        var button = $('<a href="#" role="presentation" tabindex="-1" class="button secondary postfix datetimepicker"><i class="fa fa-calendar"></i></a>');
-        var grid = $([
-            '<div class="row collapse">',
-            '<div class="' + large_column + ' columns"></div>',
-            '<div class="' + small_column + ' columns"></div>',
-            '</div>'
-        ].join(''));
+        e.preventDefault();
+        return false;
+    });
+};
 
-        var visible = false;
+var setup_datetimepicker = function(type) {
+    var locale = get_locale();
+    var i18n_options = datetimepicker_i18n[locale];
+    var selector = 'input[type=' + type + ']';
 
-        grid.insertBefore(input);
-        input.detach().appendTo(grid.find('.' + large_column));
-        button.appendTo(grid.find('.' + small_column));
-
-        // hide/show the datetime picker when clicking on the button
-        button.click(function(e) {
-            if (visible) {
-                input.datetimepicker('hide');
-            } else {
-                input.datetimepicker('show');
-            }
-
-            e.preventDefault();
-            return false;
-        });
-
-        input.datetimepicker({
-            allowBlank: true,
-            lazyInit: false,
+    var type_specific = {
+        date: {
             timepicker: false,
-            dayOfWeekStart: datetimepicker_i18n[_locale].dayOfWeekStart,
-            format: datetimepicker_i18n[_locale].format,
-            lang: datetimepicker_i18n[_locale].lang,
-            onShow: function(_current_time, $input) {
-                this.setOptions({
-                    value: $input.val()
-                });
-
-                visible = true;
+            format: i18n_options.dateformat,
+            server_to_client: function(value) {
+                return convert_date(value, 'Y-m-d', i18n_options.dateformat);
             },
-            onSelectDate: function() {
-                visible = false;
-            },
-            onClose: function() {
-                // we have to delay setting the visible flag slightly, otherwise
-                // clicking on the button when the picker is visible leads to
-                // it being hidden and shown again immediately.
-                setTimeout(function() {
-                    visible = false;
-                }, 500);
+            client_to_server: function(value) {
+                return convert_date(value, i18n_options.dateformat, 'Y-m-d');
             }
-        });
+        },
+        datetime: {
+            timepicker: true,
+            format: i18n_options.datetimeformat,
+            server_to_client: function(value) {
+
+            },
+            client_to_server: function(value) {
+
+            }
+        }
+    }[type];
+
+    var general = {
+        allowBlank: true,
+        lazyInit: false,
+        dayOfWeekStart: i18n_options.dayOfWeekstart,
+        lang: i18n_options.lang,
+        onShow: function(_current_time, $input) {
+            this.setOptions({
+                value: $input.val()
+            });
+
+            visible = true;
+        },
+        onSelectDate: function() {
+            visible = false;
+        },
+        onClose: function() {
+            // we have to delay setting the visible flag slightly, otherwise
+            // clicking on the button when the picker is visible leads to
+            // it being hidden and shown again immediately.
+            setTimeout(function() {
+                visible = false;
+            }, 500);
+        }
+    };
+
+    $(selector).each(function() {
+        var input = $(this);
+
+        attach_button(input);
+
+        input.datetimepicker($.extend(
+            general,
+            type_specific,
+        ));
+
+        // convert the initial value to the localized format
+        input.attr('placeholder', i18n_options.placeholder);
+        input.val(type_specific.server_to_client(input.val()));
 
         // remove all default on-focus events, to only show the picker when
         // clicking on the button
         input.unbind();
-
-        input.attr('placeholder', datetimepicker_i18n[_locale].placeholder);
-        input.val(convert_date(input.val(), 'Y-m-d', datetimepicker_i18n[_locale].format));
     });
 
     $('form').submit(function() {
-        $(this).find('input[type=date]').each(function() {
-            $(this).val(convert_date($(this).val(), datetimepicker_i18n[_locale].format, 'Y-m-d'));
+        $(this).find(selector).each(function() {
+            $(this).val(type_specific.client_to_server($(this).val()));
             return true;
         });
         return true;
     });
+};
+
+// load the datepicker for date inputs if the browser does not support it
+if (!Modernizr.inputtypes.date) {
+    setup_datetimepicker('date');
+}
+
+// load the datetimepicker for date inputs if the browser does not support it
+if (!Modernizr.inputtypes.datetime) {
+    setup_datetimepicker('datetime');
 }

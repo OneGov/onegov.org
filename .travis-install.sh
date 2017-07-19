cdaa21fe031ac88317e653b781a6caec99d964b9
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2017-07-19 08:44:33 +0200

    Use a chromedriver pre-release to get it running in headless mode on travis

--- .travis-install.sh
+++ .travis-install.sh
@@ -4,3 +4,7 @@ pip install tox
 if [ "$TOXENV" = 'py34' ]; then
     pip install coveralls
 fi
+
+# needed until chromedriver 2.31 is officially released
+curl -L 'https://github.com/davidthornton/chromedriver-2.31/blob/master/chromedriver?raw=true' -o /tmp/chromedriver
+chmod +x /tmp/chromedriver

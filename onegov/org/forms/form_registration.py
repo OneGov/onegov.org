6bbbb55e5462a2e89f4bb20e031e43ea71c25bda
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2018-03-27 10:40:36 +0200

    Adds registration window validation

--- onegov/org/forms/form_registration.py
+++ onegov/org/forms/form_registration.py
@@ -1,8 +1,9 @@
-from onegov.form import Form
+from cached_property import cached_property
+from onegov.form import Form, FormDefinition
 from onegov.org import _
 from wtforms.fields import BooleanField, RadioField
 from wtforms.fields.html5 import DateField, IntegerField
-from wtforms.validators import NumberRange, InputRequired
+from wtforms.validators import NumberRange, InputRequired, ValidationError
 
 
 class FormRegistrationWindowForm(Form):
@@ -64,3 +65,49 @@ class FormRegistrationWindowForm(Form):
         obj.overflow = self.waitinglist.data == 'yes'
         obj.limit = self.limit_attendees.data == 'yes' and self.limit.data or 0
         obj.enabled = not self.stop.data
+
+    @cached_property
+    def claimed_spots(self):
+        return self.model.claimed_spots
+
+    @cached_property
+    def requested_spots(self):
+        return self.model.requested_spots
+
+    def validate_limit(self, field):
+
+        # new registration windows do not need to enforce limits
+        if isinstance(self.model, FormDefinition):
+            return
+
+        # nor do unlimited registration windows
+        if not self.limit_attendees.data:
+            return
+
+        if self.limit.data < self.claimed_spots:
+            raise ValidationError(_(
+                "The limit cannot be lower than the already confirmed "
+                "number of attendees (${claimed_spots})",
+                mapping={
+                    'claimed_spots': self.claimed_spots
+                }
+            ))
+
+        # without a waitinglist we guarantee that all people who sent in
+        # an attendance request are getting a spot (unless they are maunally
+        # cancelled), so we need to take requested spots into account
+        if self.waitinglist.data == 'yes':
+            return
+
+        if self.limit.data < (self.requested_spots + self.claimed_spots):
+            raise ValidationError(_(
+                "The limit cannot be lower than the already confirmed "
+                "number attendees (${claimed_spots}) and the number of "
+                "pending requests (${pending_requests}). Either enable the "
+                "waiting list, process the pending requests or increase the "
+                "limit. ",
+                mapping={
+                    'claimed_spots': self.claimed_spots,
+                    'pending_requests': self.requested_spots
+                }
+            ))
